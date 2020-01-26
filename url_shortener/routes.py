8f1f562148c47ac00658780a8de56a8231b374d5
alexandrujeman <alexandrujeman.ja@gmail.com>
2020-01-26 22:34:05 +0200

    Add routes

--- url_shortener/routes.py
+++ url_shortener/routes.py
@@ -1,13 +1,19 @@
 from flask import Blueprint
 from flask import render_template
 from flask import request
+from flask import redirect
+from . extensions import db
+from . models import Link
 
 short = Blueprint('short', __name__)
 
 
 @short.route('/<short_url>')
 def redirect_to_url(short_url):
-    pass
+    link = Link.query.filter_by(short_url=short_url).first_or_404()
+    link.visits += 1
+    db.session.commit()
+    return redirect(link.original_url)
 
 
 @short.route("/")
@@ -17,12 +23,17 @@ def index():
 
 @short.route("/add_link", methods=["POST"])
 def add_link():
-    pass
+    original_url = request.form['original_url']
+    link = Link(original_url=original_url)
+    db.session.add(link)
+    db.session.commit()
+    return render_template('link_added.html', new_link=link.short_url, original_url=link.original_url)
 
 
 @short.route("/stats")
 def stats():
-    pass
+    links = Link.query.all()
+    return render_template('stats.html', links=links)
 
 
 @short.errorhandler(404)
